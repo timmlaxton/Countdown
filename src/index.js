a60f5ebb230beeed13ddf6a95c67f7ae3532f797
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-13 14:04:12 +0000

    rendered message through App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App';
 
 ReactDOM.render(
-    <div>Countdown champ</div>, document.getElementById('root')
+    <App></App>, 
+    document.getElementById('root')
 )
