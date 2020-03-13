2b8c4050d2e5a701ee75e5d5b1f3e38161608483
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-13 14:09:58 +0000

    added placeholder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React, {Component} from 'react';
 class App extends Component {
     render() {
         return (
-        <div>Countdown Champ, App Component</div>
+        <div>
+            <div>Countdown to March 13, 2020</div>
+            <div>
+                <div>14 days</div>
+                <div>30 hours</div>
+                <div>15 minutes</div>
+                <div>20 seconds</div>
+                <input placeholder="new date"/>
+            <button>Submit</button>
+            </div>
+        </div>
         )
     }
 }
