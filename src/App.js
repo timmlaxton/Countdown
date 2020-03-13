198557550a5098f3a3eecc5502b9887811ef89c4
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-13 14:31:52 +0000

    added css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
-        <div>
-            <div>Countdown to March 13, 2020</div>
+            <div className="App">
+            <div className="App-title">Countdown to March 13, 2020</div>
             <div>
-                <div>14 days</div>
-                <div>30 hours</div>
-                <div>15 minutes</div>
-                <div>20 seconds</div>
+                <div className="Clock-days">14 days</div>
+                <div className="Clock-hours">30 hours</div>
+                <div className="Clock-minutes">15 minutes</div>
+                <div className="Clock-seconds">20 seconds</div>
+                </div>
+                <div>
                 <input placeholder="new date"/>
-            <button>Submit</button>
+                <button>Submit</button>
             </div>
-        </div>
-        )
+            </div>
+        
+            
+           
+        
+        );
     }
 }
 
